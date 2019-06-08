731942ccae9e55d81543c35b326c1b476f3d52d0
Rafa <rafasaladar@hotmail.com>
2019-06-08 19:09:19 +0200

    inicio pruebas en php

---
+++ pie.php
@@ -0,0 +1,57 @@
+        <!--================ start footer Area  =================-->	
+        <footer class="footer-area p_120">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3  col-md-6 col-sm-6">
+                        <div class="single-footer-widget ab_wd">
+                            <h6 class="footer_title">Información</h6>
+                            <p>Politica de Cookies</p>
+							<p>Aviso Legal</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-6">
+                        <div class="single-footer-widget contact_wd">
+                            <h6 class="footer_title">Contacto</h6>
+                            <p>historiasdemazmorra@hotmail.com</p>		
+                        </div>
+                    </div>							
+                    <div class="col-lg-5 col-md-6 col-sm-6 offset-lg-1">
+                        <div class="single-footer-widget">
+                            <h6 class="footer_title">BOLETÍN</h6>
+                            <p>Solo enviamos correos informativos sobre las aventuras de rol.</p>		
+                            <div id="mc_embed_signup">
+                                <form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="subscribe_form relative">
+                                    <div class="input-group d-flex flex-row">
+                                        <input name="EMAIL" placeholder="Introduce tu email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Dirección de Correo '" required="" type="email">
+                                        <button class="btn sub-btn"><span class="lnr lnr-arrow-right"></span></button>		
+                                    </div>									
+                                    <div class="mt-10 info"></div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row footer-bottom d-flex justify-content-between align-items-center">
+                    <p class="col-lg-8 col-md-8 footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                    <div class="col-lg-4 col-md-4 footer-social">
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+		<!--================ End footer Area  =================-->
+        
+        
+        
+        
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="js/jquery-3.2.1.min.js"></script>
+        <script src="js/popper.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
