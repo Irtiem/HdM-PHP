23d440c86e1c97589998b05ecb4a8aff48625c37
Rafa <rafasaladar@hotmail.com>
2019-06-10 20:47:12 +0200

    cosas varias

--- subir.php
+++ subir.php
@@ -6,8 +6,89 @@
 		$pagina= $_GET['p'];	
 ?>
 
+
         
-	<h1>SUBIR AVENTURAS</h1>
+        <!--================Services Area =================-->
+        <section class="services_area p_120">
+        	<div class="container">
+        		<div class="main_title">
+        			<h2>Subir Modulos de Rol</h2>
+					<p>Envia un pdf a hdmazmorra@hotmail.com</p>
+        		</div>
+        		<div class="row services_inner">
+        			<div class="col-lg-4">
+        				<div class="services_item">
+        					<img src="img/icon/pdf.jpg" alt="">
+        					<a href="#"><h4>PDF de Aventuras</h4></a>
+        					<p>Aventuras de todo tipo de roles y ambientaciones.</p>
+        				</div>
+        			</div>
+        			<div class="col-lg-4">
+        				<div class="services_item">
+        					<img src="img/icon/gema.jpg" alt="">
+        					<a href="#"><h4>Totalmente Gratis</h4></a>
+        					<p>Contenido totalmente gratuito y fácil de conseguir.</p>
+        				</div>
+        			</div>
+        			<div class="col-lg-4">
+        				<div class="services_item">
+        					<img src="img/icon/grim.jpg" alt="">
+        					<a href="#"><h4>Lore y mucho más</h4></a>
+        					<p>Noticias, modulos e historias del mundo del rol.</p>
+        				</div>
+        			</div>
+        		</div>
+        	</div>
+        </section>
+
+
+        
+
+        
+        <!--================Feature Area =================-->
+        <section class="feature_area p_120">
+        	<div class="container">
+        		<div class="main_title">
+        			<h2>¿Puedo subir mis aventuras?</h2>
+        			<p>
+Si, en Historias de mazmorra buscamos varias aventuras de rol para que todos podamos disfrutarlas, sean de los roles que sean. Si estas interesado en subir tu propia aventura, 
+mandanos un pdf con tu nombre y la aventura a esta dirección hdmazmorra@hotmail.com. Tran examinarla, revisar el contenido y si todo esta en regla, la publicaremos en la web junto a las demás.
+					</p>
+        		</div>
+        		<div class="row feature_inner">
+        			<div class="col-lg-4 col-md-6">
+        				<div class="feature_item">
+        					<h4><i class="lnr lnr-user"></i>Mucha Variedad</h4>
+        					<p>Encontrarás partidas de muchos roles distintos que quizás no conozcas.</p>
+        				</div>
+        			</div>
+        			<div class="col-lg-4 col-md-6">
+        				<div class="feature_item">
+        					<h4><i class="lnr lnr-license"></i>Calidad</h4>
+        					<p>Todas las aventuras y modulos han sido revisados para asegurar su contenido.</p>
+        				</div>
+        			</div>
+        			<div class="col-lg-4 col-md-6">
+        				<div class="feature_item">
+        					<h4><i class="lnr lnr-phone"></i>Gran Comunidad</h4>
+        					<p>Esperamos disponer de una gran comunidad para crear partidas increibles.</p>
+        				</div>
+        			</div>
+        		</div>
+        	</div>
+        </section>
+        <!--================End Feature Area =================-->
+        
+
+
+
+
+
+
+
+
+
+
 <?php
 	include ("pie.php");
 ?>
